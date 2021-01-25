ec5d237c4e38fe90bed8f0d042237797e5b57f1a
Alexander <thezerothousand@gmail.com>
2021-01-25 15:48:31 -0600

    created links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {Route} from 'react-router-dom'
+import Clock from './Components/Clock/Clock';
+import Contact from './Components/Contact/Contact';
+import Welcome from './Components/Welcome/Welcome';
+import Navigation from './Components/Navigation/Navigation'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Route path="/" 
+        exact 
+        render= {(...props) => <Welcome name='Alexander'/>} 
+      />
+      <Route path="/clock" component={Clock} />
+      <Route path="/contact" component={Contact} />
     </div>
   );
 }
